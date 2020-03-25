b3192a23bed793da8236fb076c2b70a67440dfc6
Crowdbotics <team@crowdbotics.com>
2020-03-25 14:54:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fish_race_15056/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fish_race_15056;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "fish_race_15056";
+    }
+}
